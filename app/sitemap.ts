108bd91d7c1cda60d00d8b8f18b3c638469e864c
Juan D. Petter <petter2025us@outlook.com>
2026-03-18 13:27:23 -0400

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://arf-frontend-sandy.vercel.app'
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/dashboard`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/history`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
